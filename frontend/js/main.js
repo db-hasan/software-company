e9142bafc3d5e91d6903e27e4411eea6b2c49ca7
db-hasan <infoalihasanbd@gmail.com>
2024-06-09 18:55:30 +0600

    update

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,3 +1,21 @@
+var myCarousel = new bootstrap.Carousel(document.getElementById('myCarousel'), {
+    interval: 2000, // Set the interval for automatic sliding (in milliseconds)
+    wrap: true,     // Set to false if you don't want the carousel to loop
+});
+
+// menu button  start==============================
+$(document).ready(function () {
+    // jQuery to toggle cart visibility
+    $('#menuButton').click(function () {
+        $('#menuContainer').css('display', 'block');
+    });
+
+    $('#closeMenu').click(function () {
+        $('#menuContainer').css('display', 'none');
+    });
+});
+
+
 // portfolio tab Content
 $(document).ready(function () {
     // Initially show all cards
