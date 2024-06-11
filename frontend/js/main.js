9b20da2efe0a21ca4a762ef0007cba7fe612091d
db-hasan <infoalihasanbd@gmail.com>
2024-06-11 23:16:10 +0600

    update data

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,7 +1,3 @@
-var myCarousel = new bootstrap.Carousel(document.getElementById('myCarousel'), {
-    interval: 2000, // Set the interval for automatic sliding (in milliseconds)
-    wrap: true,     // Set to false if you don't want the carousel to loop
-});
 
 // menu button  start==============================
 $(document).ready(function () {
